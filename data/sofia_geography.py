e432a2b942ee61dc44f18ea53691bc7abe2ffa3f
Nikola Tulechki <nikola.tulechki@ontotext.com>
2023-05-22 15:26:57 +0300

    sofia ploygons for individual elections

--- data/sofia_geography.py
+++ data/sofia_geography.py
@@ -12,26 +12,36 @@ queryArgs = {
 
 basePath = "/home/nikola/projects/dgood/semanticElections/data/"
 
-#MI 2019 polygons
+#MI 2019-2022 polygons
 basePath = "/home/nikola/projects/dgood/semanticElections/data/"
-infile = "/home/nikola/projects/dgood/semanticElections/data/static/sections/sofia_geography/mi2019.joined.csv"
+infile = "/home/nikola/projects/dgood/semanticElections/data/static/sections/sofia_geography/section_regions/regions2019_2022.tsv"
 
 tarql.execute_query(basePath+"tarql/sections_sofia_geography.tarql",
                     infile,
-                    basePath+"rdf/geo/mi2019_sofia_wkt.ttl",
-                    '-d ; -H',
+                    basePath+"rdf/geo/2019-2022_sofia_wkt.ttl",
+                    '-t',
                     queryArgs)
 
-#SOFPLAN GE / NEIGHBORHOODS
-
+#MI 2023 polygons
 basePath = "/home/nikola/projects/dgood/semanticElections/data/"
-infile = "/home/nikola/projects/dgood/semanticElections/data/static/sections/sofia_geography/raw/ge_sofplan.csv"
+infile = "/home/nikola/projects/dgood/semanticElections/data/static/sections/sofia_geography/section_regions/regions2023.tsv"
 
-tarql.execute_query(basePath+"tarql/neighborhoods_sofia.tarql",
+tarql.execute_query(basePath+"tarql/sections_sofia_geography.tarql",
                     infile,
-                    basePath+"rdf/geo/neighborhoods_sofia_wkt.ttl",
-                    '-d ,',
+                    basePath+"rdf/geo/pi2023_sofia_wkt.ttl",
+                    '-t',
                     queryArgs)
 
-
+# #SOFPLAN GE / NEIGHBORHOODS
+#
+# basePath = "/home/nikola/projects/dgood/semanticElections/data/"
+# infile = "/home/nikola/projects/dgood/semanticElections/data/static/sections/sofia_geography/raw/ge_sofplan.csv"
+#
+# tarql.execute_query(basePath+"tarql/neighborhoods_sofia.tarql",
+#                     infile,
+#                     basePath+"rdf/geo/neighborhoods_sofia_wkt.ttl",
+#                     '-d ,',
+#                     queryArgs)
+#
+#
 
---
+++ data/static/sections/sofia_geography/work/join_wkt.py
@@ -0,0 +1,25 @@
+from shapely.geometry import Polygon
+from shapely.ops import unary_union
+from shapely import wkt
+import csv, re
+
+with open("converted_regions/mi2019.csv") as file:
+    next(csv.reader(file))
+    temp = {}
+    for row in csv.reader(file):
+        m = re.search("\((..)\)",row[2])
+        if m:
+            reg = m.group(1)
+            sec = row[3].zfill(3)
+            regsec = reg+sec
+            if regsec in temp.keys():
+               temp[regsec].append(wkt.loads(row[0]))
+            else:
+               temp[regsec]=[wkt.loads(row[0])]
+
+for regsec in temp.keys():
+    joined = unary_union(temp[regsec])
+    print(regsec+";"+joined.wkt)
+
+
+
